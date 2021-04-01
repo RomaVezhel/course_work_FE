bc03adb63079c60ce3ef1ace91623bc5cb7369db
RomaVezhel <romavezhel@gmail.com>
2021-04-01 17:13:42 +0300

    added src and package.json files

---
+++ src/specs/test.spec.js
@@ -0,0 +1,10 @@
+// const {browser} = require("protractor");
+//
+//
+// describe("Mailinator email box creation", function () {
+//     it("Load mailinator page", function () {
+//         browser.sleep(5000);
+//        browser.get("https://promo7.floralital-marketing.com/funnel_qa_course_work");
+//
+//     });
+// });
