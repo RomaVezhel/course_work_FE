0c487a19c8ca0e26d59762044b36ef8dca50ed56
RomaVezhel <romavezhel@gmail.com>
2021-06-25 07:14:44 +0300

    split test

--- src/specs/signUpNewCustomer.spec.js
+++ src/specs/signUpNewCustomer.spec.js
@@ -2,14 +2,13 @@ const FunnelPage = require("../pages/FunnelPage.js");
 const constants = require("../resources/constants");
 const singUpUserData = require("../test_data/signUpUserData.js");
 
-
 describe("Sign up new customer", function () {
   it("Check URL params", async function () {
     await FunnelPage.get();
     await expect(constants.url).toContain("funnel_qa_course_work");
   });
 
-  it("Set sign up value and submit", async function () {
+  it("Registered new lead", async function () {
     await FunnelPage.setFirstName(singUpUserData.firstName);
     await FunnelPage.setLastName(singUpUserData.lastName);
     await FunnelPage.setPhoneNum(singUpUserData.phoneNumber);
@@ -18,10 +17,10 @@ describe("Sign up new customer", function () {
     await expect(FunnelPage.popVisible()).toBeTruthy();
     await expect(FunnelPage.popUpContentText1()).toEqual(constants.popText.popUpNrpText1);
     await expect(FunnelPage.popUpContentText2()).toEqual(constants.popText.popUpNrpText2);
+  });
+
+  it("Check that pop-up is closed", async function () {
     await FunnelPage.closePopUpBtn();
     await expect(FunnelPage.popInvisible()).toBeTruthy();
   });
 });
-
-
-
